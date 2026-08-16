6cea0432731b7e605b71e618eb1a27a866b1f7db
REM <esmaeilireza1994@gmail.com>
2026-08-16 08:26:36 +0330

    chore: add public directory with placeholder files

---
+++ components/Board.tsx
@@ -0,0 +1,8 @@
+export default function Board() {
+  return (
+    <section className="p-4">
+      <h2 className="text-xl font-bold">Repository Board</h2>
+      {/* Add your board logic here */}
+    </section>
+  );
+}
